17009a1b2a1123a5a17c1c711f608c359e943346
quad400 <adedijiabdulquadri@gmail.com>
2023-10-26 01:39:04 +0100

    build.sh updated

--- config/build.sh
+++ config/build.sh
@@ -2,11 +2,11 @@
 
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
 
-/opt/venv/bin/pip install pip --upgrade && \
-/opt/venv/bin/pip install -r requirements.txt && \
+pip install pip --upgrade && \
+pip install -r requirements.txt && \
 
-/opt/venv/bin/python manage.py migrate --noinput && \
-/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true && \
+python manage.py migrate --noinput && \
+python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true && \
 
-/opt/venv/bin/python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
