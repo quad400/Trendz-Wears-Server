5a79598ee165d0b039a15e3d72c1a99bb2df8b45
quad400 <adedijiabdulquadri@gmail.com>
2023-11-01 01:25:52 +0100

    admin images updated

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,27 @@
+import admin_thumbnails
 from django.contrib import admin
 
 from .models import Product, ProductColor, ProductImage, ProductSizes
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(ProductColor)
-admin.site.register(ProductImage)
-admin.site.register(ProductSizes)
+
+@admin_thumbnails.thumbnail("image")
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    readonly_fields = ("id",)
+
+class ProductColorInline(admin.TabularInline):
+    model = ProductColor
+    extra = 1
+    readonly_fields = ("id",)
+
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSizes
+    extra = 1
+    readonly_fields = ("id",)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "category"]
+    inlines = [ProductImageInline, ProductSizeInline, ProductColorInline]
+
+admin.site.register(Product, ProductAdmin)
